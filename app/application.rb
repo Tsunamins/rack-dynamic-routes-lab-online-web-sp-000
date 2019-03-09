2d53760ad8053c7a64dd11ebb41951fec16b339e
Alison Reilly <reillyamr@gmail.com>
2019-03-09 03:03:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,7 @@ class Application
       item_name = req.path.split("/items/").last
       
         if @@items.include?(item_name)
+          binding.pry
           item = @@items.find{|i| i.name == item_name}
      
           resp.write item.price
