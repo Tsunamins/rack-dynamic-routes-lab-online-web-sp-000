af0022d7e44eb51113fdb24d5a014aca075e7d1f
Alison Reilly <reillyamr@gmail.com>
2019-03-09 03:36:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,14 +10,17 @@ class Application
     
     if req.path.match(/items/)
       
+      
+      
       item_name = req.path.split("/items/").last
       
-        item = @@items.find{|i| i.name == item_name}
+      item = @@items.find{|i| i.name == item_name}
       
           resp.write item.price
+        else 
           resp.write "Item not found"
             resp.status 400
-          end 
+           
         end 
         
      
