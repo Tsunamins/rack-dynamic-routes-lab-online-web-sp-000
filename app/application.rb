77b7c54df4668c550721333f6d7b4c7cdc50eea4
Alison Reilly <reillyamr@gmail.com>
2019-03-09 02:41:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,24 +5,16 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
-    
-    
-    #some other miscellaneous stuff I was trying
-    # #item_name = req.params["item_name"]
-    # if req.path=="/items/#{item_name}"
-    
-    
-    #what sort of works but not really
+   
+  
     
     if req.path.match(/items/)
       
       item_name = req.path.split("/items/").last
       
-      item = @@items.find{|i| i.name
-        if @@items.include?(item_req) 
-          price = item_req.price
-          resp.write "#{price}"
+      item = @@items.find{|i| i.name == item_name}
+      resp.write item.price
+        
         else 
           resp.write "Item not found"
           resp.status 400
