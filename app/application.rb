de025071b7fedba33e9926f7df28660185d9cd57
Alison Reilly <reillyamr@gmail.com>
2019-03-09 03:27:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,8 +11,8 @@ class Application
     if req.path.match(/items/)
       
       item_name = req.path.split("/items/").last
-      if item_name !=nil
-          item = @@items.find{|i| i.name == item_name}
+      item = @@items.find{|i| i.name == item_name}
+      if item != nil
           resp.write item.price
         else
             resp.write "Item not found"
