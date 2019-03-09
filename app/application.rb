9af21d097ced4d2ce3d66273aca43f45449b1027
Alison Reilly <reillyamr@gmail.com>
2019-03-09 02:43:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,10 +15,10 @@ class Application
       item = @@items.find{|i| i.name == item_name}
       resp.write item.price
         
-        else 
-          resp.write "Item not found"
-          resp.status 400
-        end
+        # else 
+        #   resp.write "Item not found"
+        #   resp.status 400
+        # end
     else
       resp.write "Route not found"
       resp.status = 404
