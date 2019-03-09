138002cecef7bb70eb5a01f4517fe725b10945b9
Alison Reilly <reillyamr@gmail.com>
2019-03-09 03:12:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
       if item_name != nil
           item = @@items.find{|i| i.name == item_name}
           resp.write item.price
-        else 
+        elsif item_name == nil
             resp.write "Item not found"
             resp.status 400
         end
