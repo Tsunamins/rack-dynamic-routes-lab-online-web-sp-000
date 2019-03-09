27a637f97100eb9c70e5f39818610b725a21ac11
Alison Reilly <reillyamr@gmail.com>
2019-03-09 02:52:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,11 +12,19 @@ class Application
       
       item_name = req.path.split("/items/").last
       
+      if @@items.include?(item_name) 
+        @@items.find do |i|
+          i.name == item_name
+          resp.write i.price
+        end 
+      else 
+        resp.write "Item not found"
+          resp.status 400
+      
       item = @@items.find{|i| i.name == item_name}
       binding.pry
       if item == nil
-        resp.write "Item not found"
-          resp.status 400
+        
         
       else 
         resp.write item.price
